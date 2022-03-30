daa4137947927bd759e4bb294d23c5548842dc6d
Takikato09 <liandelacruz09@hotmail.com>
2022-03-30 19:31:24 +0200

    Revert "Revert "Merge branch 'main' into forum-testing-2""
    This reverts commit 773636fe8c3ef3c2b38523747a0f9b2d37653936.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
 import { onValue, ref, push, remove } from "firebase/database";
-import { db, getBeerInDb, getFoodInDb, getWineInDb, user } from "./modules/firebaseApp"; 
+import { db, getBeerInDb, getFoodInDb, getWineInDb, logIn } from "./modules/firebaseApp"; 
 
 getBeerInDb();
 getFoodInDb();
 getWineInDb(console.log)
 user(); 
+
+// Login-form
+const logInUser: HTMLInputElement = document.querySelector('#log-in-name');
+const logInPassword: HTMLInputElement = document.querySelector('#log-in-pass');
+const logInBtn: HTMLButtonElement = document.querySelector('.log-in-btn');
+
+let inputUserName: String; 
+let inputPassword: String; 
+
+// Button for log in
+logInBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    inputUserName = logInUser.value;
+    inputPassword = logInPassword.value;
+    logIn(inputUserName,inputPassword, onLoginFinish);
+})
+
+// When log in is successful
+function onLoginFinish(result: false | User) {
+    console.log(result)
+
+    // Sara puts some code in here later
+
+}
