c374f7a2a4ce84922bc90f4f272a4c16f4dff534
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2022-03-30 14:41:55 +0200

    Fix log in
    Fixed log in and connecting with database to match user

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,34 @@
 import { onValue, ref, push, remove } from "firebase/database";
-import { db, getBeerInDb, getFoodInDb, getWineInDb, user } from "./modules/firebaseApp"; 
+import { db, getBeerInDb, getFoodInDb, getWineInDb, logIn } from "./modules/firebaseApp"; 
 
 getBeerInDb();
 getFoodInDb();
 getWineInDb()
-user(); 
+
+// Login-form
+const logInUser: HTMLInputElement = document.querySelector('#log-in-name');
+const logInPassword: HTMLInputElement = document.querySelector('#log-in-pass');
+const logInBtn: HTMLButtonElement = document.querySelector('.log-in-btn');
+
+let inputUserName: String; 
+let inputPassword: String; 
+
+// Button for log in
+logInBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    inputUserName = logInUser.value;
+    inputPassword = logInPassword.value;
+    logIn(inputUserName,inputPassword, onLoginFinish);
+})
+
+// When log in is successful
+function onLoginFinish(result: false | User) {
+    console.log(result)
+
+    // Sara puts some code in here later
+
+}
+
+
+
+
