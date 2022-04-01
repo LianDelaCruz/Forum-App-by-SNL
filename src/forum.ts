64cd70f95e237b22911d12e48fba1e1c52849eec
Takikato09 <liandelacruz09@hotmail.com>
2022-04-01 16:03:54 +0200

    Forum Success!
    I managed to show the forum in the console

--- src/forum.ts
+++ src/forum.ts
@@ -1,15 +1,27 @@
 
-import { getBeerInDb} from "./modules/firebaseApp";
+import { getBeerInDb, getFoodInDb, getWineInDb} from "./modules/firebaseApp";
 import { Topic } from "./modules/forumClass";
 import { displayTopic } from "./modules/display";
 
 const beerBtn:HTMLButtonElement = document.querySelector('#beer-btn');
 const foodBtn:HTMLButtonElement = document.querySelector('#food-btn');
 const wineBtn:HTMLButtonElement = document.querySelector('#wine-btn');
-const topicContainer = document.querySelector("#topic-container")
+
 
 beerBtn.addEventListener('click',e => {
     e.preventDefault();
    getBeerInDb(displayTopic)
+});
+
+foodBtn.addEventListener('click', e => {
+    e.preventDefault();
+    getFoodInDb(displayTopic);
+});
+
+wineBtn.addEventListener('click', e => {
+    e.preventDefault();
+    getWineInDb(displayTopic);
 })
 
+
+export { beerBtn, foodBtn, wineBtn }
