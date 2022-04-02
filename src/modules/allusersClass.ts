188e99cc55790fef5e2155947a844c1ee793d9e8
SaraM83 <60971822+SaraM83@users.noreply.github.com>
2022-04-02 15:07:39 +0200

    Hover effect
    Started making the hover effect showing users profiles.

--- src/modules/allusersClass.ts
+++ src/modules/allusersClass.ts
@@ -6,20 +6,36 @@ export class AllUsers {
     ){
         this.displayAllUsers(allUsers);
     }
-    private displayAllUsers(allUsers):void { /// displays all users from database, called when instans created 
-            for(const key in allUsers) {
-                console.log(allUsers[key].bio, allUsers[key].username)
-                const usersContainer:HTMLDivElement = document.createElement('div'); 
-                document.body.append(usersContainer); 
-                const userPar:HTMLParagraphElement = document.createElement('p'); 
-                userPar.innerText = allUsers[key].username; 
-                usersContainer.appendChild(userPar);
-                const bioPar:HTMLParagraphElement = document.createElement('p'); 
-                bioPar.classList.add('#bio'); 
-                //bioPar.style.display = ''; // hidden until hovered
-                bioPar.innerText = allUsers[key].bio; 
-                usersContainer.appendChild(bioPar);  
-            }
+    private displayAllUsers(allUsers):void { /// displays all users from database, 
+        for(const key in allUsers) {
+
+            console.log(allUsers[key].bio, allUsers[key].username)
+            const usersContainer:HTMLDivElement = document.createElement('div'); 
+            document.body.append(usersContainer); 
+            const userParName:HTMLParagraphElement = document.createElement('p'); 
+            userParName.innerText = allUsers[key].username; 
+            userParName.style.fontStyle = 'bold'; 
+            usersContainer.appendChild(userParName);
+
+            const hiddenDiv:HTMLDivElement = document.createElement('div'); 
+            document.body.append(hiddenDiv); 
+            hiddenDiv.style.display = 'none'; 
+            const bioPar:HTMLParagraphElement = document.createElement('p'); 
+            bioPar.classList.add('#bio'); 
+            //bioPar.style.display = ''; // hidden until hovered
+            bioPar.innerText = allUsers[key].bio; 
+            hiddenDiv.appendChild(bioPar);  
+            const img:HTMLImageElement = document.createElement('img');
+            hiddenDiv.appendChild(img); 
+            img.src = allUsers[key].img; 
+            userParName.addEventListener('mouseover', function() {
+                hiddenDiv.style.display = 'block'; 
+                    
+            })
+            userParName.addEventListener('mouseout', function() {
+                hiddenDiv.style.display = 'none'; 
+            })
+        }
     
     }
 }
